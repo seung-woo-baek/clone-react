1ff115b11824880d3acb5551f812003f02374f6e
백승우 <swbaek@gooroomee.com>
2023-02-11 16:56:24 +0900

    함수 component, 클래스 component 만들기 및 virtual Dom 로직

--- react.js
+++ react.js
@@ -1,3 +1,8 @@
+export class Component{
+  constructor(props){
+    this.props = props
+  }
+}
 
 export function createDOM(node){
   if(typeof node === 'string'){
@@ -16,12 +21,47 @@ export function createDOM(node){
   return element;
 }
 
+const makeProps = (props, children) => {
+  return {
+    ...props,
+    children: children.length === 1 ? children[0] : children
+  }
+}
+
 export function createElement(tag, props, ...children){
   props = props || {};
+
+  if(typeof tag === 'function'){
+    if(tag.prototype instanceof Component){
+      const instance = new tag(makeProps(props, children))
+      return instance.render();
+    }
+
+    if(children.length > 0){
+      return tag(makeProps(props, children))
+    } else{
+      return tag();
+    }
+  }
 
   return { tag, props, children };
 }
 
-export function render(vdom, container){
-  container.appendChild(createDOM(vdom));
-}
+// export function render(vdom, container){
+//   container.appendChild(createDOM(vdom));
+// }
+
+export const render = (() => {
+  let prevDom = null;
+
+  return function(vdom, container){
+    if(prevDom === null){
+      prevDom = vdom;
+    }
+
+    //diff
+    //가상돔의 변경점만 비교해서 교체 해준 뒤 렌더링(변경점 넣는 로직 이 위치에 넣어야 함)
+    
+    container.appendChild(createDOM(vdom));
+  }
+})();
